a974c075cf9975c133c729acb6d15f40b853f92b
Midrise <62632932+midriseab@users.noreply.github.com>
2024-02-21 16:46:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$(function() {
+  
+  function format(v) {
+    return (v.toString().length == 1) ? '0' + v : v;
+  }
+  
+  let currentDate = Date.now();
+  const endDateString = "2024-02-21T16:43:50";
+  const endDateTime = Date.parse(endDateString);
+  const endDate = new Date(endDateTime);
+  
+  const $days = $('.days');
+  const $hours = $('.hours');
+  const $mins = $('.minutes');
+  const $secs = $('.seconds');
+  
+  setInterval(function() {
+    
+    currentDate = Date.now();
+    if (currentDate < endDate) {
+    
+      const time = endDate - currentDate;
+    
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 60000) % 60);
+      const hours = Math.floor((time / 3600000) % 24);
+      const days = Math.floor((time / 86400000));
+    
+      $secs.text( format(seconds) );
+      $mins.text( format(minutes) );
+      $hours.text( format(hours) );
+      $days.text( days );
+      
+   } else {
+      window.location.href = "./abilday.html";
+   }
+    
+  }, 100);
+  
+});
